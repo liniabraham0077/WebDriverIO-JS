3a75d5d62ff9eaa480900837b623a781c16ef081
Lini Abraham <liniabraham007@gmail.com>
2023-05-20 14:24:19 +1000

    added waituntil documentation

--- test/specs/e2e.js
+++ test/specs/e2e.js
@@ -7,6 +7,7 @@ describe('Test Suite', async()=>{
         await $('#username').setValue("rahulshettyacademy");
         await $('#password').setValue("123learning");
         await $('#signInBtn').click();
+        // https://webdriver.io/docs/api/browser/waitUntil/
         await browser.waitUntil(async() => await $('#signInBtn').getAttribute('value') === 'Sign In', {
             timeout: 5000,
             timeoutMsg: 'expected text to be different after 5s'
