9077f5dfbb2bf4de36fb672a9ee577e08a1e5852
Lini Abraham <liniabraham007@gmail.com>
2023-05-23 10:40:44 +1000

    added test to handle radio buttons

---
+++ test/specs/login.js
@@ -0,0 +1,25 @@
+describe('Test Suite', async()=>{
+    it('error on entering incorrect password', async()=>{
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise")
+        await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
+        await $('#username').setValue("rahulshettyacademy");
+        await $('#password').setValue("123learning");
+        await $('#signInBtn').click();
+        await browser.waitUntil(async() => await $('#signInBtn').getAttribute('value') === 'Sign In', {
+            timeout: 5000,
+            timeoutMsg: 'expected text to be different after 5s'
+        })
+        console.log(await $('.alert-danger').getText())
+    })
+
+    it('login successfully', async()=>{
+        await browser.url("https://rahulshettyacademy.com/loginpagePractise")
+        await expect(browser).toHaveTitleContaining('Rahul Shetty Academy')
+        await $('#username').setValue("rahulshettyacademy");
+        await $('#password').setValue("learning");
+        await $('#signInBtn').click();
+        await $(".btn-primary").waitForExist()
+        await expect(browser).toHaveUrlContaining("shop");
+        await expect(browser).toHaveTitle("ProtoCommerce");
+    })
+})
