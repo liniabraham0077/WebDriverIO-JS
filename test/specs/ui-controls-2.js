f00d5d584327a5cd9d79ea694dd9c540dd698fc9
Lini Abraham <liniabraham007@gmail.com>
2023-05-24 22:10:35 +1000

    added Promise.all when there are multiple awaits

--- test/specs/ui-controls-2.js
+++ test/specs/ui-controls-2.js
@@ -2,7 +2,7 @@ import { expect as expectChai } from 'chai'
 import { expect as expectWDIO } from '@wdio/globals'
 
 describe('UI controls test', async() => {
-    it('scrolling and mouse hover', async() => {
+    xit('scrolling and mouse hover', async() => {
         await browser.maximizeWindow();
         await browser.url("https://rahulshettyacademy.com/AutomationPractice")
         // await browser.$("#mousehover").scrollIntoView({ block: 'nearest', inline: 'nearest' })
@@ -12,5 +12,19 @@ describe('UI controls test', async() => {
         await browser.pause(5000)
         await $("=Top").click()
         await browser.pause(5000)
+    })
+
+    it('handle tables', async() => {
+        const arr = [1,2,3,4,5]
+        await browser.maximizeWindow();
+        await browser.url("https://rahulshettyacademy.com/seleniumPractise/#/offers")
+        await $('tr th:first-child').click();
+        const veggiesColumn =  await $$("tr td:nth-child(1)")
+
+        const actualVeggiesList = await Promise.all(await veggiesColumn.map(async veggie => await veggie.getText()))
+        const veggiesList =   actualVeggiesList.slice()
+        //Arrays are pass by reference
+        const expectedVeggiesList =  veggiesList.sort()
+        expectChai(actualVeggiesList).to.eql(expectedVeggiesList)
     })
 })
